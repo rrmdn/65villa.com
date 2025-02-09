554bb417c513b0d410af1b75e82c0281d9fdd28a
Rizki Romadhoni <rrmdn@protonmail.com>
2025-02-09 13:01:55 +0100

    load translations properly

--- src/i18n/loadLocale.ts
+++ src/i18n/loadLocale.ts
@@ -1,6 +1,6 @@
 export default async function loadLocale(locale: string) {
   const messages = await fetch(
-    `https://hoomanize.com/api/projects/c76fe7eb-4ed6-49b3-accd-9adb839b9310/releases/latest/d14710fe-f5ba-4056-938c-1f232137f512/json/${locale}.json`
+    `https://i18n-cdn.lokaliku.com/c76fe7eb-4ed6-49b3-accd-9adb839b9310/releases/latest/d14710fe-f5ba-4056-938c-1f232137f512/json/${locale}.json`
   ).then((resp) => resp.json());
   return messages;
 }
